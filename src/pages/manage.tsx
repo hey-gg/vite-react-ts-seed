1f96c7cbe32dfe8fcd55804701188c1ca48cc412
heyong <1012334358@qq.com>
2024-04-01 19:32:15 +0800

    feat:  路由重定向

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -2,11 +2,18 @@
  * @Author: heyong
  * @Date: 2024-04-01 18:36:13
  * @LastEditors: heyong
- * @LastEditTime: 2024-04-01 18:36:20
+ * @LastEditTime: 2024-04-01 19:30:36
  */
+import { Navigate } from 'react-router-dom';
+
 const Manage = () => {
-    return <div>管理页</div>;
-  };
-  
-  export default Manage;
-  
+  // 情景：当没有权限时，重定向到登录页  replace={true}：进行重定向
+  if (!localStorage.getItem('token')) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  return <div>manage</div>;
+};
+
+export default Manage;
+
