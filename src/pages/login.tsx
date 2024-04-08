888e655d3c1e30d43d21053c54c81d13804e310d
heyong <1012334358@qq.com>
2024-04-08 09:59:15 +0800

    feat: 配置antd

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,17 @@
  * @Author: heyong
  * @Date: 2024-04-01 18:13:34
  * @LastEditors: heyong
- * @LastEditTime: 2024-04-01 18:13:49
+ * @LastEditTime: 2024-04-08 09:58:13
  */
+import { Button } from 'antd';
+
 const Login = () => {
-    return <div>登录页</div>;
+    return (
+        <div>
+            登录页
+            <Button type="primary">Button</Button>
+        </div>
+    );
 };
 
 export default Login;
