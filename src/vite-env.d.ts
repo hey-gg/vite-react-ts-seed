e16f41ad71616b21878d499abc3169c6bdf98408
heyong <1012334358@qq.com>
2024-04-08 10:15:36 +0800

    feat: 配置环境变量

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
+/*
+ * @Author: heyong
+ * @Date: 2024-04-01 15:52:38
+ * @LastEditors: heyong
+ * @LastEditTime: 2024-04-08 10:02:57
+ */
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_BASE_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
